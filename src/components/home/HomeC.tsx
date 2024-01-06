192e6b00effdb76c9239ec8daf6f029af1baf31d
kaiqueZpriv1 <kakamelo141205@gmail.com>
2024-01-06 16:59:44 -0300

    Create home com svg animações e estilos

---
+++ src/components/home/HomeC.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+	PrompIa,
+	Cerebro,
+	HomeS,
+	HomeText,
+	Line1,
+	Line2,
+	Map,
+	Invisible,
+} from './style';
+import TypewriterComponent from 'typewriter-effect';
+import LineSvg from './svg/Vector.svg';
+import CerebroSvg from './svg/cerebro.svg';
+import PrompSvg from './svg/AI Prompt.svg';
+import MapBr from './svg/BR Map.svg';
+export const HomeC = () => {
+	return (
+		<HomeS>
+			<div className='container-home'>
+				<HomeText>
+					<h3 className='text-primary'>Seja bem vindo, me chamo</h3>
+					<h1 className='name-home'>
+						&lt;
+						<TypewriterComponent
+							options={{
+								strings: ['Kaique Ferreira'],
+								autoStart: true,
+								loop: true,
+								skipAddStyles: true,
+								cursor: '',
+							}}
+						/>
+						/&gt;
+					</h1>
+					<p className='description-home'>Desenvolvedor Front-End</p>
+				</HomeText>
+			</div>
+			<Line1>
+				<img src={LineSvg} alt='line' className='line-svg-left' />
+				<img src={LineSvg} alt='line' className='line-svg1-left' />
+				<img src={LineSvg} alt='line' className='line-svg2-left' />
+			</Line1>
+			<Line2>
+				<img src={LineSvg} alt='line' className='line-svg-right' />
+				<img src={LineSvg} alt='line' className='line-svg1-right' />
+				<img src={LineSvg} alt='line' className='line-svg2-right' />
+			</Line2>
+			<Cerebro>
+				<img src={CerebroSvg} alt='cerebro' className='cerebro-svg-right' />
+			</Cerebro>
+			<PrompIa>
+				<img src={PrompSvg} alt='promp' className='promp-svg' />
+			</PrompIa>
+			<Map>
+				<img src={MapBr} alt='br mapa' className='br-map' />
+			</Map>
+			<Invisible>
+				<h3 className='text-invisible'>Confiança</h3>
+				<h3 className='text-invisible'>Profissionalismo</h3>
+				<h3 className='text-invisible'>Segurança</h3>
+				<h3 className='text-invisible'>Responsabilidade</h3>
+			</Invisible>
+		</HomeS>
+	);
+};
---
+++ src/components/home/style.ts
@@ -0,0 +1,142 @@
+import styled from 'styled-components';
+
+export const HomeS = styled.main`
+	width: 100%;
+	padding: 200px 100px 0 100px;
+	.container-home {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+        position: absolute;
+        left: 0;
+	}
+`;
+
+export const HomeText = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	align-items: center;
+	justify-content: center;
+    position: relative;
+    z-index: 8;
+	.text-primary {
+		font-size: 2.5em;
+		color: #bfcfe7;
+		font-size: 900;
+		text-align: center;
+	}
+	.name-home {
+		display: flex;
+		gap: 10px;
+		font-size: 3.5em;
+		font-weight: 900;
+		color: #00a7e1;
+	}
+	.description-home {
+		font-size: 2em;
+		color: #bfcfe7;
+		font-size: 500;
+	}
+`;
+
+export const Line1 = styled.div`
+	.line-svg-left {
+		position: absolute;
+		transform: rotate(-30deg);
+		top: 60px;
+		z-index: 8;
+		width: 140px;
+	}
+	.line-svg1-left {
+		position: absolute;
+		transform: rotate(-30deg);
+		top: 60px;
+		z-index: 8;
+		width: 115px;
+		left: 130px;
+	}
+	.line-svg2-left {
+		position: absolute;
+		transform: rotate(-30deg);
+		top: 60px;
+		z-index: 8;
+		width: 90px;
+		left: 160px;
+	}
+`;
+export const Line2 = styled.div`
+	.line-svg-right {
+		position: absolute;
+		transform: rotate(150deg);
+		bottom: -15px;
+		z-index: 8;
+		width: 140px;
+		right: 100px;
+	}
+	.line-svg1-right {
+		position: absolute;
+		transform: rotate(150deg);
+		bottom: -15px;
+		z-index: 8;
+		width: 115px;
+		right: 130px;
+	}
+	.line-svg2-right {
+		position: absolute;
+		transform: rotate(150deg);
+		bottom: -15px;
+		z-index: 8;
+		width: 90px;
+		right: 160px;
+	}
+`;
+
+export const Cerebro = styled.div`
+    .cerebro-svg-right{
+        width: 120px;
+        position: absolute;
+        top: 100px;
+        right: 80px;
+    }
+`
+
+export const PrompIa = styled.div`
+    .promp-svg{
+        width: 150px;
+        position: absolute;
+        left: 100px;
+        bottom: 40px;
+    }
+`
+
+export const Map = styled.div`
+    .br-map{
+        position: absolute;
+        z-index: 7;
+        width: 550px;
+        inset: 100px 400px;
+        transform: rotate(-7deg);
+    }
+`
+
+export const Invisible = styled.div`
+    width: 100%;
+    padding: 30px 100px;
+    background-color: rgba(1, 1, 14, 1);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+    bottom: -83px;
+    left: 0;
+    /* box-shadow: 0 0 5px #fff; */
+    z-index: 8;
+    .text-invisible{
+        font-size: 1em;
+        color: #00a7e1;
+        font-weight: 600;
+    }
+`
