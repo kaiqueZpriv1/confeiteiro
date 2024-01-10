0328bbd4e5420a710d721bf6eca64a33908bb32e
kaiqueZpriv1 <kakamelo141205@gmail.com>
2024-01-10 00:05:43 -0300

    create e estilização da pagina habilidades

--- src/components/Habilidades/HabilidadeC.tsx
+++ src/components/Habilidades/HabilidadeC.tsx
@@ -1,12 +1,72 @@
-import React from "react";
-import { BoxingHabilidade, HabilidadesS } from "./style";
+import React from 'react';
+import { BoxingHabilidade, HabilidadesS, InvisibleHab, LineHab } from './style';
 
+import Html from './image/icon-html.png';
+import Css from './image/icon-css.png';
+import Js from './image/icon-js.png';
+import ReactJS from './image/icon-reactjs.png';
+import Git from './image/icon-git.png';
+import Github from './image/icon-github.png';
+import LineSvg from '../home/svg/Vector.svg';
 export const HabilidadeC = () => {
-    return(
-        <HabilidadesS>
-            <div className="container-habilidade">
-                <BoxingHabilidade></BoxingHabilidade>
-            </div>
-        </HabilidadesS>
-    )
-}
+	const listHabilidade = [
+		{
+			id: 0,
+			img: Html,
+			title: 'Html',
+			color: '#fc490b',
+		},
+		{
+			id: 1,
+			img: Css,
+			title: 'Css',
+			color: '#2196f3',
+		},
+		{
+			id: 2,
+			img: Js,
+			title: 'JavaScript',
+			color: '#ffdf00',
+		},
+		{
+			id: 3,
+			img: ReactJS,
+			title: 'React Js',
+			color: '#39c1d7',
+		},
+		{
+			id: 4,
+			img: Git,
+			title: 'Git',
+			color: '#ed4a40',
+		},
+		{
+			id: 5,
+			img: Github,
+			title: 'Git-Hub',
+			color: '#555e63',
+		},
+	];
+	return (
+		<HabilidadesS>
+			<InvisibleHab/>
+			<LineHab>
+				<img src={LineSvg} alt='line' className='line-svg-left' />
+				<img src={LineSvg} alt='line' className='line-svg1-left' />
+				<img src={LineSvg} alt='line' className='line-svg2-left' />
+			</LineHab>
+			<div className='container-habilidade'>
+				{listHabilidade.map((i) => {
+					return (
+						<BoxingHabilidade key={i.id}>
+							<img src={i.img} alt='tec' className='image-hbl' />
+							<h3 className='title-hbl' style={{ color: i.color }}>
+								{i.title}
+							</h3>
+						</BoxingHabilidade>
+					);
+				})}
+			</div>
+		</HabilidadesS>
+	);
+};
--- src/components/Habilidades/style.ts
+++ src/components/Habilidades/style.ts
@@ -5,21 +5,66 @@ export const HabilidadesS = styled.section`
 	padding: 200px 0;
 	.container-habilidade {
 		display: grid;
-		grid-template-columns: 200px 200px 200px 200px;
+		grid-template-columns: 200px 200px 200px;
 		gap: 30px;
+		align-items: center;
+		justify-content: center;
 	}
 `;
 
 export const BoxingHabilidade = styled.div`
-	padding: 20px;
-	border-radius: 15px;
-	border: 2px solid #00a7e1;
+	padding: 30px;
+	border-radius: 20px;
+	border: 3px solid #00a7;
+	box-shadow: 0 0 5px #00a7e1;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 10px;
 	.image-hbl {
-		width: 120px;
+		width: 100px;
+		cursor: pointer;
 	}
 	.title-hbl {
 		font-size: 1.5em;
-		color: #bfcfe7;
 		text-align: center;
 	}
+`;
+
+export const LineHab = styled.div`
+	.line-svg-left {
+		position: relative;
+		transform: rotate(-30deg);
+		top: 60px;
+		z-index: 7;
+		width: 140px;
+		left: 100px;
+	}
+	.line-svg1-left {
+		position: relative;
+		transform: rotate(-30deg);
+		top: 40px;
+		z-index: 7;
+		width: 115px;
+		left: -10px;
+	}
+	.line-svg2-left {
+		position: relative;
+		transform: rotate(-30deg);
+		top: 20px;
+		z-index: 7;
+		width: 90px;
+		left: -95px;
+	}
+`;
+
+export const InvisibleHab = styled.div`
+	width: 120px;
+	background-color: rgba(1, 1, 14, 1);
+	height: 50px;
+	position: relative;
+	top: 75px;
+	left: 120px;
+	z-index: 8;
 `;
