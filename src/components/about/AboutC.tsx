c5d604cf0222a2d0797eb2e1864c605f36652949
kaiqueZpriv1 <kakamelo141205@gmail.com>
2024-01-14 21:17:04 -0300

    correção em about

--- src/components/about/AboutC.tsx
+++ src/components/about/AboutC.tsx
@@ -10,12 +10,12 @@ export const AboutC = () => {
 					<h3 className='title-about'>Kaique</h3>
 					<p className='paragrafo-about'>
 						Tenho 18 anos e sou um desenvolvedor Front-End na cidade de São
-						Paulo. Estou no meu último ano do ensino médio e busco me formar em
-						Análise e Desenvolvimento de Sistemas. Meu primeiro contato com o
-						desenvolvimento web foi em 2022, quando comecei a estudar e realizar
-						meus primeiros projetos em HTML, CSS e JavaScript. Atualmente,
-						estudo e desenvolvo projetos como freelancer, buscando evoluir e
-						ampliar meus conhecimentos em Front-End.
+						Paulo. Busco me formar em Análise e Desenvolvimento de Sistemas. Meu
+						primeiro contato com o desenvolvimento web foi em 2022, quando
+						comecei a estudar e realizar meus primeiros projetos em HTML, CSS e
+						JavaScript. Atualmente, estudo e desenvolvo projetos como
+						freelancer, buscando evoluir e ampliar meus conhecimentos em
+						Front-End.
 					</p>
 					<ButtonAbout>
 						<a
